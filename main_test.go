a0617a045551830556c39b65e9f5a0db7bb29497
MarkusSeiberl <markus_seiberl@live.at>
2020-05-27 20:00:18 +0200

    fetches db credentials from environment variables

--- main_test.go
+++ main_test.go
@@ -14,14 +14,10 @@ import (
 var a App
 
 func TestMain(m *testing.M) {
-	/*a.Initialize(
-	os.Getenv("APP_DB_USERNAME"),
-	os.Getenv("APP_DB_PASSWORD"),
-	os.Getenv("APP_DB_NAME"))*/
 	a.Initialize(
-		"postgres",
-		"postgres",
-		"postgres")
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
 	ensureTableExists()
 	code := m.Run()
