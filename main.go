22aa5ae1f2e709da15c69ca270794e1abf57ce12
MarkusSeiberl <markus_seiberl@live.at>
2020-05-27 19:22:03 +0200

    removes build script; adds hardcoded db credentials

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"os"
-)
-
 func main() {
 	a := App{}
+	/*a.Initialize(
+	os.Getenv("APP_DB_USERNAME"),
+	os.Getenv("APP_DB_PASSWORD"),
+	os.Getenv("APP_DB_NAME"))*/
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"postgres",
+		"products")
 
 	a.Run(":8010")
 }
--- main_test.go
+++ main_test.go
@@ -14,10 +14,14 @@ import (
 var a App
 
 func TestMain(m *testing.M) {
+	/*a.Initialize(
+	os.Getenv("APP_DB_USERNAME"),
+	os.Getenv("APP_DB_PASSWORD"),
+	os.Getenv("APP_DB_NAME"))*/
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"postgres",
+		"products")
 
 	ensureTableExists()
 	code := m.Run()
